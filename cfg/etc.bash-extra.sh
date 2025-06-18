738a6d7d54dac0d92cd1adfac911a05de0a5ad62
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2025-06-17 22:39:31 -0300

    Fix: root PS1.

--- cfg/etc.bash-extra.sh
+++ cfg/etc.bash-extra.sh
@@ -3,6 +3,6 @@ alias ls="ls -AFh --full-time --group-directories-first --color"
 alias ll="ls -l"
 
 # make PATH literal/expanded
-export PS1="\u@\h:\${PWD}\n\$ "
+export PS1="\u@\h:\${PWD}\n\\$ "
 
 export PGDATA=/var/lib/postgresql
