76af58cde9e4dc247feed58c6feca9df489442ba
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2025-06-24 00:26:50 -0300

    Use a bash array to index the list of products, instead of 'cat -n/grep' to identify each line from DICT file.

--- bin/insert-random-orders.sh
+++ bin/insert-random-orders.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Source of products
-# from MacOS/BSD
-DICT="data/usr-share-dict-web2a.txt"
-_qtd_lines=$( wc -l ${DICT} | awk '{print $1}' | tr -d ' ')
-
-
 # db-primary
 export PGDATABASE=postgres
 export PGHOST=127.0.0.1
@@ -15,7 +9,23 @@ export PGUSER=tst
 export PGPASSWORD=tst
 
 
+# Source of products
+# from MacOS/BSD
+DICT="data/usr-share-dict-web2a.txt"
+_qtd_lines=$( wc -l ${DICT} | awk '{print $1}' | tr -d ' ')
 
+_dict_item=() # bash array
+_line_no=1    # array index
+while IFS= read -r line
+do  # each file line_no is now the array index
+    _dict_item[${_line_no}]="$line"
+    (( _line_no++ ))
+done < ${DICT}
+
+
+##
+## Run it
+##
 while true
 do
   # Batch: amount of INSERTS
@@ -27,12 +37,18 @@ do
 
   for i in $(seq 1 ${_ins_size})
   do
-    # Generate data
-    _dict_line=$( shuf -n 1 -i 1-${_qtd_lines} )
-    _dict_name=$( cat -n ${DICT} | grep -w ${_dict_line} | awk '{print $2,$3}' )
+    # Generate random product
 
-    _prod_qtty=$( shuf -n 1 -i 1-10 )
+    # Pick a random line number from DICT file
+    _dict_line=$( shuf -n 1 -i 1-${_qtd_lines} )
+    # v1: grep the line by number
+#   _dict_name=$( cat -n ${DICT} | grep -w ${_dict_line} | awk '{print $2,$3}' )
+    # v2: get the line number from array
+    _dict_name=${_dict_item[${_dict_line}]}
     _prod_name=${_dict_name^} # Bash: Capitalize
+
+    # Generate random quantity
+    _prod_qtty=$( shuf -n 1 -i 1-10 )
 
     printf "$(date '+%F %X') Insert: [%02d] Qty: %2d, Product: ${_prod_name}\n" ${i} ${_prod_qtty}
 
